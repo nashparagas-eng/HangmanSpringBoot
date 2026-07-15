a1dda8b6049c93818566af34069422a5b0c93d07
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-07-15 19:05:22 +0800

    Initial commit

---
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/HangmanSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ph.edu.dlsu.lbycpob.hangmanspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HangmanSpringBootApplication.class, args);
+    }
+
+}
