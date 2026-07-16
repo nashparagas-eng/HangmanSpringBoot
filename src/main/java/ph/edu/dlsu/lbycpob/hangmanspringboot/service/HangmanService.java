28175c2bf616c75f9ac8cd204c53bfe58ba48adf
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-07-16 22:59:29 +0800

    Added imports to HangmanService

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/service/HangmanService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/service/HangmanService.java
@@ -1,4 +1,16 @@
 package ph.edu.dlsu.lbycpob.hangmanspringboot.service;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import ph.edu.dlsu.lbycpob.hangman.render.HangmanRenderer;
+import ph.edu.dlsu.lbycpob.hangman.repository.WordRepository;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+import java.util.Random;
+
 public class HangmanService {
 }
