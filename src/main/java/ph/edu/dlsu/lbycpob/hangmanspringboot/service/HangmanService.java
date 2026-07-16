a025789934f9b2fba587eb98d1a1be89efac8c20
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-07-16 22:59:47 +0800

    Added HangmanService function

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/service/HangmanService.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/service/HangmanService.java
@@ -12,5 +12,18 @@ import java.util.List;
 import java.util.Objects;
 import java.util.Random;
 
+/**
+ * Pure, stateless game-logic service.
+ */
+@Service
 public class HangmanService {
-}
+
+    private static final Logger log = LoggerFactory.getLogger(HangmanService.class);
+
+    /** Maximum incorrect guesses before the player loses. */
+    public static final int MAX_GUESSES = 8;
+
+    private static final String[] FALLBACK_WORDS = {
+            "JAVA", "HANGMAN", "COMPUTER", "KEYBOARD", "PROGRAM", "ALGORITHM"
+    };
+
