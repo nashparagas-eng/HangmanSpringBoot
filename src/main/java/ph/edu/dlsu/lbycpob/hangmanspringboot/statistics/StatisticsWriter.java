9139040d9ec6f2b00a17c77a48e65687b9af63a9
Nash Paragas <nash_paragas@dlsu.edu.ph>
2026-07-16 23:02:24 +0800

    Added imports to StatisticsWriter

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/statistics/StatisticsWriter.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/statistics/StatisticsWriter.java
@@ -1,4 +1,12 @@
 package ph.edu.dlsu.lbycpob.hangmanspringboot.statistics;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class StatisticsWriter {
 }
