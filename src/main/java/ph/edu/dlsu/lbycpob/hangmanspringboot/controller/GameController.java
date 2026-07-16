8952ad20e337158c5551d8e6abcab098008be151
12507393 <francis_martin_ang@dlsu.edu.ph>
2026-07-16 13:05:53 +0800

    pasted the imports and package for this class

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/controller/GameController.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/controller/GameController.java
@@ -1,4 +1,15 @@
-package ph.edu.dlsu.lbycpob.hangmanspringboot.controller;
+package ph.edu.dlsu.lbycpob.hangman.controller;
 
-public class GameController {
-}
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import ph.edu.dlsu.lbycpob.hangman.model.GameState;
+import ph.edu.dlsu.lbycpob.hangman.service.HangmanService;
+import ph.edu.dlsu.lbycpob.hangman.statistics.GameStatistics;
+import ph.edu.dlsu.lbycpob.hangman.statistics.StatisticsWriter;
+
+
+public class GameController {}
