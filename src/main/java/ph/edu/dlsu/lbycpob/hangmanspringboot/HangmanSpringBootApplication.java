e16b718f047c8578597b3db5f12ae1739ac70290
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-19 00:56:22 +0800

    Add the basic interface structure for rendering the hangman game stateSet up the package location and import Spring Boot startup tools

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/HangmanSpringBootApplication.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/HangmanSpringBootApplication.java
@@ -1,20 +1,5 @@
-package ph.edu.dlsu.lbycpob.hangmanspringboot;
+package ph.edu.dlsu.lbycpob.hangman;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import ph.edu.dlsu.lbycpob.hangman.render.AsciiArtRenderer;
-import ph.edu.dlsu.lbycpob.hangman.render.HangmanRenderer;
-import ph.edu.dlsu.lbycpob.hangman.repository.ClasspathWordRepository;
-import ph.edu.dlsu.lbycpob.hangman.repository.WordRepository;
-
-import java.util.Random;
-
-@SpringBootApplication
-public class HangmanSpringBootApplication {
-
-    public static void main(String[] args) {
-        SpringApplication.run(HangmanSpringBootApplication.class, args);
-    }
-
-}
