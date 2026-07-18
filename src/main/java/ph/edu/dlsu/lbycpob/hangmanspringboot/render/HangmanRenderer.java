78817325c584e9e14d0a5a8675dc9e85666f5de3
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-19 00:37:41 +0800

    Add HangmanRenderer interface in the render package

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/render/HangmanRenderer.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/render/HangmanRenderer.java
@@ -3,3 +3,9 @@ package ph.edu.dlsu.lbycpob.hangman.render;
 import java.io.IOException;
 import java.util.List;
 
+/**
+ * Produces the hangman drawing for a given number of guesses remaining.
+ */
+public interface HangmanRenderer {
+
+
