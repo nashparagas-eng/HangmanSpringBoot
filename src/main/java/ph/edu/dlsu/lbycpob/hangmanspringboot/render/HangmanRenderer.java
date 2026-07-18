eb351075898d3c83e1ede7250a25eab20f0be1b9
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-19 00:38:07 +0800

    Add the basic interface structure for rendering the hangman game state

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/render/HangmanRenderer.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/render/HangmanRenderer.java
@@ -8,4 +8,10 @@ import java.util.List;
  */
 public interface HangmanRenderer {
 
-
+    /**
+     * Returns the lines of hangman art for {@code guessesRemaining} (0–8).
+     *
+     * @throws IOException if the art resource cannot be located or read
+     */
+    List<String> render(int guessesRemaining) throws IOException;
+}
