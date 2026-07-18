5e2cd5b68399e02fb58a50030e0286072b740d4b
Joshua <joshua_balingit@dlsu.edu.ph>
2026-07-19 00:23:59 +0800

    Import standard Java tools for reading streams, handling errors, and managing text formats

--- src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/utils/ClassPathResources.java
+++ src/main/java/ph/edu/dlsu/lbycpob/hangmanspringboot/utils/ClassPathResources.java
@@ -2,3 +2,8 @@ package ph.edu.dlsu.lbycpob.hangman.utils;
 
 import ph.edu.dlsu.lbycpob.hangman.render.AsciiArtRenderer;
 import ph.edu.dlsu.lbycpob.hangman.repository.ClasspathWordRepository;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
